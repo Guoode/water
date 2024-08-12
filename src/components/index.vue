f0bd941795e6869a3c97dc6b94ab23c3c0832b9c
sheep <319788742@qq.com>
2024-08-12 11:36:54 +0800

    2

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <h1>{{ msg }}</h1>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+      <el-footer>Footer</el-footer>
+    </el-container>
   </div>
 </template>
 
@@ -12,3 +19,29 @@ export default {
   },
 };
 </script>
+<style>
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
+import '@/assets/css/global.css';
 
 const app = createApp(App)
 
