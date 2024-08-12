cea8b91fdbfd673512c783e97dc65d1e8b20c049
sheep <319788742@qq.com>
2024-08-12 19:01:24 +0800

    2

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,8 +1,5 @@
 <template>
-  <div>
-    <el-icon> <Edit /></el-icon>
-    <el-button type="primary" class="el-icon-search">查询</el-button>
-  </div>
+  <div>Main</div>
 </template>
 
 <script>
@@ -11,13 +8,4 @@ export default {
 };
 </script>
 
-<style scoped>
-.el-icon-user {
-  font-size: 30px;
-  color: green;
-}
-.el-icon-search {
-  font-size: 20px;
-  color: black;
-}
-</style>
+<style scoped></style>
