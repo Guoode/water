2b7e59bce13e4ceb6809e1d0223d731ec54c46d4
sheep <319788742@qq.com>
2024-08-13 10:17:05 +0800

    111

--- src/router/index.js
+++ src/router/index.js
@@ -7,12 +7,18 @@ import index from "../components/index.vue"
 const routes = [
   {
     path: "/login",
-    component: Login
+    component:()=>import('../components/Login.vue'),
   },
   {
     path: "/index",
-    component: index
-  }
+    component:()=>import('../components/index.vue'),
+  },
+  {
+    //实现路由重定向，当进入网页时，路由自动跳转到/student路由
+    redirect:'/login',
+    path:'/'
+}
+
 ];
 
 const router = createRouter({
